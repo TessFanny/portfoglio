a88b992a0d7f364851b4c862e0fd59932a431b48
TessFanny <fanitess@yahoo.fr>
2023-10-23 20:13:27 +0200

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({})],
-  optimizeDeps: {
-    entries: [
-      'src/index.html',
-      'main/main.jsx',
-    ]
-  },
-  build: {
-    rollupOptions: {
-      input: "src/main.js",
-    },
+  plugins: [react()],
+  // build: {
+  //   lib: {
+  //     // Could also be a dictionary or array of multiple entry points
+  //     entry: resolve(__dirname, 'lib/main.jsx'),
+  //     formats: ['es']
+  //   },
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+        
+  //     },
+  //     external: ['react'],
+  //   },
     
-  },
+  // },
 
 
 });

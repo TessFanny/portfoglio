f3165223ee9384f38cebb242e371eab0be17f7b8
TessFanny <fanitess@yahoo.fr>
2023-10-23 15:29:46 +0200

    about page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'build', // Specify the output folder
+  },
+  server: {
+    host: true
+  },
+  rollupOptions: {
+    input: 'src/main.jsx',
+    
+  }
 })

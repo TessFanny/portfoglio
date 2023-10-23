b004a95e0090a24b7ee0230ac0a60bb81e9bdcf6
TessFanny <fanitess@yahoo.fr>
2023-10-23 16:20:25 +0200

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({})],
   optimizeDeps: {
-    include: ['linked-dep'],
+    entries: [
+      'src/index.html',
+      'main/main.jsx',
+    ]
   },
   build: {
     rollupOptions: {
       input: "src/main.js",
     },
-    // commonjsOptions: {
-    //   include: [/linked-dep/, /node_modules/],
-    // },
+    
   },
 
 
